8ea513732b3319c64c84a1f37126d0665b7c3c70
aristo-13 <oseirichmond310@gmail.com>
2023-11-10 20:48:58 +0000

    new commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,22 +1,27 @@
 import ecom from '../assets/ecom.svg'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-6'>
         <div className=' h-20 w-20 flex items-center'>
+             <button className='hidden md:block'><MenuIcon /></button>
             <img src={ecom} alt="logo" className='w-full h-full object-cover'/>
-            <span className='text-3xl font-semibold'>ECOM</span>
+            <span className='text-3xl font-semibold block md:hidden'>ECOM</span>
         </div>
-        <ul className='capitalize flex items-center gap-10 lg:text-lg text-2xl cursor-pointer'>
-            <li>home</li>
-            <li>Products</li>
-            <li>Purchase</li>
-            <li>settings</li>
-        </ul>
+        <div className='block md:hidden'>
+            <ul className='capitalize flex items-center gap-10 lg:text-lg text-2xl cursor-pointer'>
+                <NavLink to='/'>home</NavLink>
+                <NavLink>Products</NavLink>
+                <NavLink to='/purchase'>Purchase</NavLink>
+                <NavLink>settings</NavLink>
+            </ul>
+        </div>
         <div className='flex items-center gap-4'>
-            <div className=' bg-slate-200 rounded-full px-4 py-2 flex items-center'>
+            <div className=' bg-slate-200 rounded-full px-4 py-2 flex items-center md:hidden'>
                 <label htmlFor="cart"><SearchIcon/></label>
                 <input type="search" placeholder='Search' id='cart' className='text-base border-none px-4 py-2 rounded-full bg-transparent'/>
             </div>
