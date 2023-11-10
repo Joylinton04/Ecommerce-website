bebb55efec5a0690ceee5be8e210797265c75f3e
JoyLinton04 <adujoy05@gmail.com>
2023-11-10 23:15:30 +0000

    refactor

--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-
-
 import {FaShoppingBag} from 'react-icons/fa'
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-/* =======
-import skincare from '../assets/skincare.png'
-import SwiperButton from '../component/SwiperButton';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css';
->>>>>>> 51f9b8ac8bd286ab20791c788b4396919af98e06
- */
-=======
 
-
->>>>>>> 468777e3eaf605f81c8cce767023efd8567adacd
 const Purchase = () => {
   return (
-<<<<<<< HEAD
 
     <div className='h-full'>
         <div className='flex flex-row md:flex-col'>
@@ -52,8 +33,7 @@ const Purchase = () => {
                         </div>
                     </div>
                     <main className='bg-black/10 p-3 flex flex-col gap-3'>
-                       { 
-                        items.map((item) => (
+                       { items.map((item) => (
                             <div className='flex flex-col gap-3 bg-white p-5 shadow-md md:p-2'>
                             <div className='flex justify-between p-2 border-b'>
                                 <span>TO PAY</span>
@@ -95,52 +75,5 @@ const Purchase = () => {
     </div>
     )
   }
-    /*
-=======
-    <div className='overflow-y-auto h-screen'>
-        <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            className=''
-            >
-            <SwiperSlide>
-                 <div className="px-6 py-4 h-[60vh] flex relative -z-10">
-                    <div className=" w-3/5 h-full bg-[#681f23] rounded-t-3xl flex flex-col items-center justify-around text-white">
-                        <h1 className="text-7xl font-semibold">Collection Beauty</h1>
-                        <div className="flex items-center gap-10 w-2/3">
-                            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam optio accusantium in officia provident odit aliquam eaque dicta incidunt reprehenderit inventore quaerat doloremque, doloribus exercitationem sit. Maxime expedita a quia!</p>
-                            <div>
-                                <button className="bg-white px-4 py-2 text-black text-lg rounded-full w-32">See details</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" w-2/5 h-full rounded-t-3xl border-2 relative">
-                        <img src={skincare} alt="" className='w-full h-full object-cover rounded-t-3xl'/>
-                        <div className='capitalize absolute bg-white top-[70%] left-20 px-4 py-2 rounded-lg'>
-                            <h1 className='text-lg'>Skincare</h1>
-                            <p><span className='font-bold'>category:</span> Lotion</p>
-                            <p><span className='font-bold'>Price:</span> 499$</p>
-                        </div>
-                    </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-        </Swiper>
-*/
-    
-=======
-    <div>
-        
-    </div>
-  )
-}
->>>>>>> 468777e3eaf605f81c8cce767023efd8567adacd
 
 export default Purchase;
