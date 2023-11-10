fba5c5d792099e8dc6424d7474039ee146596571
JoyLinton04 <adujoy05@gmail.com>
2023-11-10 13:18:45 +0000

    Approute

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from './component/Navbar';
 import Home from './pages/Home';
 import Footer from './component/Footer';
+import Approute from './route/Approute';
 import './App.css'
 
 const App = () => {
   return (
     <div className='min-h-screen'>
       <Navbar/>
-      <Home/>
+      <Approute/>
       <Footer/>
     </div>
   )
