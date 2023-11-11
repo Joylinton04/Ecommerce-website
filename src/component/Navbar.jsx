9e07dbba2ebd198414d4f08c6c1fa685bcc3b9f1
JoyLinton04 <adujoy05@gmail.com>
2023-11-11 09:48:54 +0000

    refactor

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -24,12 +24,12 @@ const Navbar = () => {
     <div className='flex justify-between items-center px-6 lg:px-2 cursor-pointer border-b'>
         <div className=' h-20 w-20 flex items-center'>
             <img src={ecom} alt="logo" className='w-full h-full object-cover'/>
-            <span className='text-3xl font-semibold' onClick={() => handleElementClick()}>ECOM</span>
+            <span className='text-3xl font-semibold'>ECOM</span>
         </div>
         <ul className='capitalize flex items-center gap-10 2xl:text-lg text-2xl cursor-pointer 2xl:gap-6'>
             <li ref={homeRef} onClick={() => handleElementClick(homeRef)} className='active'><Link to="/">Home</Link></li>
             <li ref={purchaseRef} onClick={() => handleElementClick(purchaseRef)}><Link to='/purchase'>Purchase</Link></li>
-            <li ref={settingsRef} onClick={() => handleElementClick(settingsRef)}><Link to='/settings'>Settings</Link></li>
+            {/* <li ref={settingsRef} onClick={() => handleElementClick(settingsRef)}><Link to='/settings'>Settings</Link></li> */}
         </ul>
         <div className='flex items-center gap-4'>
             <div className=' bg-slate-200 rounded-full px-4 py-2 lg:py-1 flex items-center'>
